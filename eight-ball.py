6e196967d31ec2a768120d62e5fb09043d9cd041
PRM <prmwrites@gmail.com>
2017-05-26 16:00:14 -0400

    Added basic user story

--- eight-ball.py
+++ eight-ball.py
@@ -1,2 +1,8 @@
 
-# Initialize
+# Ask user for a question
+
+# display 'in progess' message while ball is 'thinking'
+
+# randomly select one of 20 responses
+
+# allow user to ask another question, or, quit
