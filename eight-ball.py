2aa768d87a77f6ee636a25568241a3e775549769
PRM <prmwrites@gmail.com>
2017-05-26 16:09:58 -0400

    Query user for inputted question

--- eight-ball.py
+++ eight-ball.py
@@ -1,5 +1,8 @@
 
 # Ask user for a question
+question = input("Ask the Magic 8-Ball a question:\n")
+#test that input is being stored successfully
+print(question)
 
 # display 'in progess' message while ball is 'thinking'
 
