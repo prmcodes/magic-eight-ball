48f3e8b4d93aebffbe3e50cd4e40940ae7d3b5ce
PRM <prmwrites@gmail.com>
2017-06-04 19:17:42 -0400

    One of several pondering messages is randomly selected

--- eight-ball.py
+++ eight-ball.py
@@ -1,10 +1,14 @@
+import random
 
 # Ask user for a question
 question = input("Ask the Magic 8-Ball a question:\n")
 
-# display 'in progess' message while ball is 'thinking'
-pondering_dict = { 'ponder1': '.\n. .\n. . .\nReading the runes\n. . . .\n. . . . .\n' }
-print(pondering_dict['ponder1'])
+# display one random 'in progess' message of several while ball is 'thinking'
+pondering_dict = { '.\n. .\n. . .\nReading the runes\n. . .\n. .\n.': 'ponder_message_1' ,
+                   'Hmm...\nAn interesting quesiton...': 'ponder_message_2',
+                   'Well now... Let me think...': 'ponder_message_3',
+                   'Patience. I must consult the spirits...': 'ponder_message_4' }
+print(random.choice(list(pondering_dict)) + '\n')
 
 # randomly select one of 20 responses
 
