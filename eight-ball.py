96c3da1ffa5c0aba8263d41241d6331a14cb4408
PRM <prmwrites@gmail.com>
2017-06-05 21:37:31 -0400

    Changed simple variables into functions

--- eight-ball.py
+++ eight-ball.py
@@ -1,18 +1,12 @@
 import random
 import time
 
-# Ask user for a question
-question = input("Ask the Magic 8-Ball a question:\n")
-time.sleep(2)
-# display one random 'in progess' message of several while ball is 'thinking'
 pondering_dict = { '.\n. .\n. . .\nReading the runes\n. . .\n. .\n.': 'ponder_message_1' ,
                    'Hmm...\nAn interesting quesiton...': 'ponder_message_2',
                    'Well now... Let me think...': 'ponder_message_3',
                    'Patience. I must consult the spirits...': 'ponder_message_4'
                    }
-ponder_response = print('\n' + random.choice(list(pondering_dict)) + '\n')
-time.sleep(3)
-# randomly select one of 20 responses
+
 answer_dict = { 'It is certain.': 'answer_message_1',
                 'It is decidedly so.': 'answer_message_2',
                 'Without a doubt.': 'answer_message_3',
@@ -34,8 +28,27 @@ answer_dict = { 'It is certain.': 'answer_message_1',
                 'Outlook not so good.': 'answer_message_19',
                 'Very doubtful.': 'answer_message_20'
 }
-received_response = print("I've divined the answer to your question:")
-time.sleep(1)
-answer_response = print(random.choice(list(answer_dict)) + '\n')
+
+def query_question():
+  question = input("Ask the Magic 8-Ball a question:\n")
+  time.sleep(2)
+  return
+
+def ponder_response():
+  ponder_response = print('\n' + random.choice(list(pondering_dict)) + '\n')
+  time.sleep(3)
+  return
+
+def answer_response():
+  received_response = print("I've divined the answer to your question:")
+  time.sleep(1)
+  answer_response = print(random.choice(list(answer_dict)) + '\n')
+  return
+
+query_question()
+ponder_response()
+answer_response()
+
+
 
 # allow user to ask another question, or, quit
